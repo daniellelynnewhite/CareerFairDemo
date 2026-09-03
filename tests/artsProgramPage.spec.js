728d0c4bc704bb7fd6a3d9d32149da5c1b5cf295
Danielle White <daniellelynnewhite@aol.com>
2026-09-03 11:56:12 -0700

    fix-spacing

--- tests/artsProgramPage.spec.js
+++ tests/artsProgramPage.spec.js
@@ -10,7 +10,7 @@ import {
   orchestraGrades5To12PageIsLoaded,
   theatreGrades7To12PageIsLoaded,
   visualArtsGrades7To12PageIsLoaded
- } from './helperFunctions.js';
+} from './helperFunctions.js';
 
 // command to run: npx playwright test artsProgramPage.spec.js
 // command to run with UI visible: npx playwright test artsProgramPage.spec.js --debug
